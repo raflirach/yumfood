50d08caf932e439fcfa708eb30efaf4f25b01cb9
raflirach <rafli060395@gmail.com>
2021-03-09 09:37:09 +0700

    add: userSeeder & dishSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(TagSeeder::class);
         $this->call(VendorSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(DishSeeder::class);
     }
 }
