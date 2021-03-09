5c17f9ceb1f56ba0f8040cd06a1a696d9a5d3193
raflirach <rafli060395@gmail.com>
2021-03-09 09:36:02 +0700

    add: dish factory

---
+++ database/factories/DishFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Dish;
+use Faker\Generator as Faker;
+
+$factory->define(Dish::class, function (Faker $faker) {
+    $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+
+    return [
+        'name' => $faker->foodName(),
+        'vendor_id' => $faker->numberBetween($min = 1, $max = 50),
+    ];
+});
