210425a696784d23a35d87388624a1bfa683e4b9
raflirach <rafli060395@gmail.com>
2021-03-09 08:09:55 +0700

    add: model dish

---
+++ app/Dish.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dish extends Model
+{
+    protected $fillable = ['name'];
+
+    public function vendor()
+    {
+        return $this->belongsTo('App\Vendor');
+    }
+}
