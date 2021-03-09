140e759a5a24891a9cc079c6e571eada4f5167ff
raflirach <rafli060395@gmail.com>
2021-03-09 14:13:43 +0700

    add: order migration

---
+++ database/migrations/2021_03_09_061510_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->references('id')->on('users');
+            $table->bigInteger('dish_id')->references('id')->on('dishes');
+            $table->boolean('status');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
