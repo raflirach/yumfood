b10566aa6b34310c1e42363386bbab25203aa87a
raflirach <rafli060395@gmail.com>
2021-03-12 06:28:53 +0700

    add: create order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,12 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Resources\OrderResource;
 use App\Order;
+use App\Http\Requests\OrderRequest;
 
 class OrderController extends Controller
 {
     public function index()
     {
         return OrderResource::collection(Order::orderBy('id')->paginate());
+    }
+
+    public function store(OrderRequest $request)
+    {
+        $user_id = $request->user_id;
+        $dish_id = $request->dish_id;
+        $quantity = $request->quantity;
+
+        $order = Order::create([
+            "user_id" => $user_id,
+            "dish_id" => $dish_id,
+            "status" => false,
+            "quantity" => $quantity,
+        ]);
+
+        return new OrderResource($order);
     }
 }
 
